278ea0b4406f2fe40e1eee2e1c7e3b70af02c27d
PromtGamesLab <timurchik130304@gmail.com>
2026-09-18 20:41:32 +0300

    feat: добавлено получение истории сообщений

--- services/message-service/app/api/messages.py
+++ services/message-service/app/api/messages.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from app.database.dependencies import get_db
 from app.schemas.message import MessageCreate, MessageResponse
 from app.security.dependencies import get_current_user_id
-from app.services.messages import create_message
+from app.services.messages import create_message, get_chat_messages
 
 
 router = APIRouter(
@@ -28,6 +28,28 @@ async def send_message(
             db=db,
             sender_id=user_id,
             data=data,
+        )
+
+    except ValueError as error:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=str(error),
+        )
+
+@router.get(
+    "/chat/{chat_id}",
+    response_model=list[MessageResponse],
+)
+async def get_messages(
+    chat_id: int,
+    user_id: int = Depends(get_current_user_id),
+    db: Session = Depends(get_db),
+):
+    try:
+        return await get_chat_messages(
+            db=db,
+            chat_id=chat_id,
+            user_id=user_id,
         )
 
     except ValueError as error:
--- services/message-service/app/services/messages.py
+++ services/message-service/app/services/messages.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from app.clients.chat_service import check_user_chat_membership
 from app.models.message import Message
 from app.schemas.message import MessageCreate
-
+from sqlalchemy import select
 
 async def create_message(
     db: Session,
@@ -31,3 +31,29 @@ async def create_message(
     db.refresh(message)
 
     return message
+
+
+async def get_chat_messages(
+    db: Session,
+    chat_id: int,
+    user_id: int,
+) -> list[Message]:
+    is_member = await check_user_chat_membership(
+        chat_id=chat_id,
+        user_id=user_id,
+    )
+
+    if not is_member:
+        raise ValueError(
+            "Пользователь не состоит в этом чате"
+        )
+
+    statement = (
+        select(Message)
+        .where(Message.chat_id == chat_id)
+        .order_by(Message.created_at.asc())
+    )
+
+    return list(
+        db.scalars(statement).all()
+    )
