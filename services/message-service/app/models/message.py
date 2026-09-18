e6dc8ecd21ddad53d72779332114d63e3f366d4d
PromtGamesLab <timurchik130304@gmail.com>
2026-09-18 19:18:15 +0300

    feat: добавлена модель сообщений

---
+++ services/message-service/app/models/message.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime, String, Text, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database.connection import Base
+
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True
+    )
+
+    chat_id: Mapped[int] = mapped_column(
+        nullable=False,
+        index=True,
+    )
+
+    sender_id: Mapped[int] = mapped_column(
+        nullable=False,
+        index=True,
+    )
+
+    text: Mapped[str] = mapped_column(
+        Text,
+        nullable=False,
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
+
+    is_read: Mapped[bool] = mapped_column(
+        default=False,
+        nullable=False,
+    )
